c1be45bdbf192e34435bf9c01bec0956d79071b9
mond124 <raymondtaniwidjaja@gmail.com>
2024-02-12 00:05:04 +0700

    making serializer for genre

--- backend/base/api/serializers.py
+++ backend/base/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from ..models import Book
+from ..models import Book,Genre
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['name']
 
 class BookSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ['book_id', 'title', 'synopsis', 'views', 'rating', 'genres']
