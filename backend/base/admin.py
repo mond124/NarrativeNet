f16063727e50ea45bde5eb51f45b1f1ff733a570
mond124 <raymondtaniwidjaja@gmail.com>
2024-02-12 11:00:58 +0700

    editing admin panel
    make admin panel can add more than 1 genre using filter horizontal widget and change admin panel display

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Book,Genre
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Genre, GenreAdmin)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'synopsis', 'views', 'rating', 'display_genres')
+    filter_horizontal = ('genres',)  # Use filter horizontal widget for genres
+
+    def display_genres(self, obj):
+        return ", ".join([genre.name for genre in obj.genres.all()])
+
+    display_genres.short_description = 'Genres'
+
+admin.site.register(Book, BookAdmin)
