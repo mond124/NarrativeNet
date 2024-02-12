f69787c71c7a276eb46e690395d1df7a31ef19c6
mond124 <raymondtaniwidjaja@gmail.com>
2024-02-12 23:17:27 +0700

    adding text and image to database
    adding text path and image path field in database

--- backend/base/api/serializers.py
+++ backend/base/api/serializers.py
@@ -8,10 +8,12 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class BookSerializer(serializers.ModelSerializer):
     genres = serializers.SerializerMethodField()
+    text_path = serializers.CharField(source='text_path.url')
+    image_path = serializers.CharField(source='image_path.url')
 
     class Meta:
         model = Book
-        fields = ['book_id', 'title', 'synopsis', 'views', 'rating', 'genres']
+        fields = ['book_id', 'title', 'synopsis', 'views', 'rating', 'genres', 'text_path', 'image_path']
 
     def get_genres(self, obj):
         return [genre.name for genre in obj.genres.all()]
