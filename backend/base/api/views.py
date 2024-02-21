b397730510d84a219352cf653ab9e6558493db4f
mond124 <raymondtaniwidjaja@gmail.com>
2024-02-21 23:07:43 +0700

    Enhance API Functionality and Error Handling
     Implemented several improvements to the API functionality, including input validation for API endpoints, consistent error messages for duplicate book titles, and enhanced documentation with docstrings. These changes ensure better handling of invalid input data, provide clearer feedback to users, and improve code readability and maintainability.

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -10,28 +10,41 @@ from .serializers import BookSerializer, ChapterSerializer
 from django.db.models import Q
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Custom token obtain pair serializer to include username in the token payload.
+    """
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
         token['username'] = user.username
-
         return token
 
 class MyTokenObtainPairView(TokenObtainPairView):
+    """
+    Custom token obtain pair view to use the custom serializer.
+    """
     serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getBooks(request):
+    """
+    API endpoint to retrieve all books.
+    """
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def getBooksByGenre(request, genre_name):
+    """
+    API endpoint to retrieve books by genre.
+    """
     try:
         genres = [genre.strip() for genre in genre_name.split(',')]
         books = Book.objects.filter(genres__name__in=genres).distinct()        
+        books = Book.objects.filter(genres__name__in=genres).distinct()        
+        print(f"Books for genre(s) '{', '.join(genres)}': {books}")  # Debug print
+        books = Book.objects.filter(genres__name__in=genres).distinct()
         print(f"Books for genre(s) '{', '.join(genres)}': {books}")  # Debug print
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -42,49 +55,42 @@ def getBooksByGenre(request, genre_name):
 
 @api_view(['GET'])
 def searchBooks(request):
+    """
+    API endpoint to search books by title or synopsis.
+    """
     query = request.query_params.get('q', '')
     if query:
-        # Perform a case-insensitive search across title and synopsis fields
         results = Book.objects.filter(Q(title__icontains=query) | Q(synopsis__icontains=query))
-
-        # Fuzzy matching for handling typos
         titles = [book.title for book in results]
         fuzzy_results = process.extract(query, titles, limit=5)
 
-        # Filter books based on fuzzy matched titles and similarity threshold
         fuzzy_matching_results = []
         for title, similarity in fuzzy_results:
-            if similarity >= 60:  # Adjust the threshold as needed
+            if similarity >= 60:
                 book = results.get(title=title)
                 fuzzy_matching_results.append(book)
 
-        # Sort the results by similarity
         fuzzy_matching_results.sort(key=lambda x: fuzz.ratio(query.lower(), x.title.lower()), reverse=True)
-
         serializer = BookSerializer(fuzzy_matching_results, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         return Response({"detail": "Please provide a search query."}, status=status.HTTP_400_BAD_REQUEST)
 
 class BulkCreateChaptersAPIView(APIView):
+    """
+    API endpoint to bulk create chapters.
+    """
     def post(self, request, format=None):
         chapters_data = request.data if isinstance(request.data, list) else [request.data]
-
         response_data = {}
         new_chapters_data = []
-
-        # Retrieve existing chapters from the database
         existing_chapter_titles = Chapter.objects.values_list('title', flat=True)
-        
-        # Filter out chapters that are not already in the database
         for chapter_data in chapters_data:
             title = chapter_data.get('title')
             if title not in existing_chapter_titles:
                 new_chapters_data.append(chapter_data)
             else:
                 response_data.setdefault('chapters_errors', []).append(f"Chapter '{title}' already exists.")
-
-        # Create chapters for the new ones
         if new_chapters_data:
             chapters_serializer = ChapterSerializer(data=new_chapters_data, many=True)
             if chapters_serializer.is_valid():
@@ -94,25 +100,21 @@ class BulkCreateChaptersAPIView(APIView):
                 response_data.setdefault('chapters_errors', []).extend(chapters_serializer.errors)
         else:
             response_data.setdefault('chapters_errors', []).append("No new chapters to create.")
-
-        # Return the response
         return Response(response_data, status=status.HTTP_201_CREATED)
 
 class BulkCreateBooksAndChaptersAPIView(APIView):
+    """
+    API endpoint to bulk create books and chapters.
+    """
     def post(self, request, format=None):
         data = request.data
         books_data = data.get('books', [])
         chapters_data = data.get('chapters', [])
-
-        # Check if the data is for single book/chapter or bulk
         is_single_book = isinstance(books_data, dict)
         is_single_chapter = isinstance(chapters_data, dict)
-
         response_data = {}
         books_to_create = []
         chapters_to_create = []
-
-        # Check if books already exist in the database
         for book_data in books_data:
             title = book_data.get('title')
             existing_books = Book.objects.filter(title=title)
@@ -120,8 +122,6 @@ class BulkCreateBooksAndChaptersAPIView(APIView):
                 response_data.setdefault('books_errors', []).append(f"Book '{title}' already exists.")
             else:
                 books_to_create.append(book_data)
-
-        # Check if chapters already exist in the database
         for chapter_data in chapters_data:
             title = chapter_data.get('title')
             existing_chapters = Chapter.objects.filter(title=title)
@@ -129,8 +129,6 @@ class BulkCreateBooksAndChaptersAPIView(APIView):
                 response_data.setdefault('chapters_errors', []).append(f"Chapter '{title}' already exists.")
             else:
                 chapters_to_create.append(chapter_data)
-
-        # Create books if not already in the database
         if books_to_create:
             books_serializer = BookSerializer(data=books_to_create, many=True)
             if books_serializer.is_valid():
@@ -138,8 +136,6 @@ class BulkCreateBooksAndChaptersAPIView(APIView):
                 response_data['books'] = books_serializer.data
             else:
                 response_data.setdefault('books_errors', []).extend(books_serializer.errors)
-
-        # Create chapters if not already in the database
         if chapters_to_create:
             chapters_serializer = ChapterSerializer(data=chapters_to_create, many=True)
             if chapters_serializer.is_valid():
@@ -147,13 +143,14 @@ class BulkCreateBooksAndChaptersAPIView(APIView):
                 response_data['chapters'] = chapters_serializer.data
             else:
                 response_data.setdefault('chapters_errors', []).extend(chapters_serializer.errors)
-
-        # Return the response
         return Response(response_data, status=status.HTTP_201_CREATED)
 
 @api_view(['POST'])
 def createBook(request):
-    if isinstance(request.data, list):  # Check if data is a list (bulk creation)
+    """
+    API endpoint to create a book.
+    """
+    if isinstance(request.data, list):
         success_data = []
         error_data = []
         for book_data in request.data:
@@ -188,7 +185,7 @@ def createBook(request):
             "error": error_data
         }
         return Response(response_data, status=status.HTTP_201_CREATED)
-    else:  # Single book creation
+    else:
         serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
             title = request.data.get('title')
@@ -213,6 +210,9 @@ def createBook(request):
 
 @api_view(['GET'])
 def getRoutes(request):
+    """
+    API endpoint to retrieve available routes.
+    """
     routes = [
         '/api/token',
         '/api/token/refresh',
