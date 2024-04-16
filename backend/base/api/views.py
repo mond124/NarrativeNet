cd86beb834471761c3cdb8a50e719ee5416fcb66
mond124 <raymondtaniwidjaja@gmail.com>
2024-04-16 23:10:01 +0700

    importing lib from rest_framework

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework import status, generics, permissions, filters
 from rest_framework.views import APIView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
