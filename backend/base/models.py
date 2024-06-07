7d410d20d4ec77882baa517df15d72f9df7c5eec
mond124 <raymondtaniwidjaja@gmail.com>
2024-06-07 16:24:20 +0700

    adding Book models

--- backend/base/models.py
+++ backend/base/models.py
@@ -17,3 +17,15 @@ class Publisher(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    synopsis = models.TextField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    book_cover = models.ImageField(upload_to='book_covers/')
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    views = models.IntegerField()
+
+    def __str__(self):
+        return self.title
