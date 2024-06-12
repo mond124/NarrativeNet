8e04b52750f4024885b00ccb966bfd81d810898a
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-12 10:25:18 +0700

    Remove duplicated directory

--- frontend/src/components/roundedComponents/RatingRounded.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RatingRounded = () => {
-  return (
-    <div>RatingRounded</div>
-  )
-}
-
-export default RatingRounded
--- frontend/src/components/roundedComponents/ViewsRounded.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ViewsRounded = () => {
-  return (
-    <div>ViewsRounded</div>
-  )
-}
-
-export default ViewsRounded
