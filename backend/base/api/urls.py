7c49dede249b4d85e014d6e1efaeb9ce89692a8c
mond124 <raymondtaniwidjaja@gmail.com>
2024-06-20 22:39:53 +0700

    adding new url for BooksByGenreView

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import getRoutes, AddBookView, GetAllBooksView, GetBookView, SearchBookView
+from .views import getRoutes, AddBookView, GetAllBooksView, GetBookView, SearchBookView, BooksByGenreView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('books/', GetAllBooksView.as_view(), name='get-all-books'),
     path('books/<int:pk>/', GetBookView.as_view(), name='get-book'),
     path('search/', SearchBookView.as_view(), name='search-books'),
+    path('books/genre/<str:genre_name>/', BooksByGenreView.as_view(), name='books-by-genre'),
 ]
